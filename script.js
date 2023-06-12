96c1909cc40f633e87fc8ff17199bee8ed931743
Sam <samnewell91@gmail.com>
2023-06-13 00:29:36 +0100

    Added functionality to buttons
    Buttons now play sounds

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-const sounds = ["/home/deck/repos/XP-Soundboad/Sounds/Windows XP Startup.mp3"];
 
+
+$('#startUp').click(function () {
+    var audio = document.getElementById('startSound');
+    audio.play();
+});
+
+$('#lowBattery').click(function () {
+    var audio = document.getElementById('batterySound');
+
+    audio.play();
+
+});
+
+$('#tada').click(function () {
+    var audio = document.getElementById('tadaSound');
+    audio.play();
+});
+
+$('#exclamation').click(function () {
+    var audio = document.getElementById('exclamationSound');
+    audio.play();
+});
+
+$('#logOff').click(function () {
+    var audio = document.getElementById('logOffSound');
+    audio.play();
+});
