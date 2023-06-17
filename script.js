a80f7fea8aeb16c9cb35a12a73f8c60f919a87d4
Sam <samnewell91@gmail.com>
2023-06-17 10:25:12 +0100

    Added shake effect using jquery.

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-
+const button = document.get
 
 $('#startUp').click(function () {
+    $(this).effect("shake", {distance: 5});
     var audio = document.getElementById('startSound');
     audio.play();
 });
 
 $('#lowBattery').click(function () {
+    $(this).effect("shake", {distance: 5});
     var audio = document.getElementById('batterySound');
-
     audio.play();
 
 });
 
 $('#tada').click(function () {
+    $(this).effect("shake", {distance: 5});
     var audio = document.getElementById('tadaSound');
     audio.play();
 });
 
 $('#exclamation').click(function () {
+    $(this).effect("shake", {distance: 5});
     var audio = document.getElementById('exclamationSound');
     audio.play();
 });
 
 $('#logOff').click(function () {
+    $(this).effect("shake", {distance: 5});
     var audio = document.getElementById('logOffSound');
     audio.play();
 });
